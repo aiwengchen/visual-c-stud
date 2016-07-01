ac401efa7bbe16a9d2cfd788ad9fe11c5c3ca5d5
aiweng chen <aiwengchen@126.com>
2016-07-01 10:20:17 +0800

    全局与局部变量

--- BaseSample/BaseSample.cpp
+++ BaseSample/BaseSample.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
+// 范围确定符的示例
+int pages = 800;		// 全局变量
+void printPages()
+{
+	int pages = 100;    // 本地变量
+	cout << "全局变量pages=" << ::pages;	// 打印全局变量
+	cout << '\n';
+	cout << "本地变量pages=" << pages;      // 打印本地变量
+	cout << '\n';
+}
 
 int main()
 {
-    return 0;
+	printPages();
+	
+	return 0;
 }
 
